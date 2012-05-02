81244e442305c59f456c7d58e0fda477256e0b48
tbranyen <tim@tabdeveloper.com>
2012-05-01 23:20:04 -0400

    removed use plugin

--- app/config.js
+++ app/config.js
@@ -1,8 +1,9 @@
 // Set the require.js configuration for your application.
 require.config({
-  // Initialize the application with the main application file
+  // Initialize the application with the main application file.
   deps: ["main"],
 
+  // Paths are shortcuts to make require and define calls... shorter. :)
   paths: {
     // JavaScript folders
     libs: "../assets/js/libs",
@@ -11,12 +12,11 @@ require.config({
     // Libraries
     jquery: "../assets/js/libs/jquery",
     underscore: "../assets/js/libs/underscore",
-    backbone: "../assets/js/libs/backbone",
-
-    // Shim Plugin
-    use: "../assets/js/plugins/use"
+    backbone: "../assets/js/libs/backbone"
   },
 
+  // Define legacy libraries and plugins here, these are scripts which are
+  // natively incompatible with the AMD format.
   legacy: {
     backbone: {
       deps: ["underscore", "jquery"],
